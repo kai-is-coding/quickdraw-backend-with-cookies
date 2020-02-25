ff552f1ed499077d9a1b1a4a82fc5110482eceb2
Kai Yang <kaiyang.ky2@gmail.com>
2020-02-25 21:01:06 +1100

    heroku deploy debugging test

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,9 @@ Rails.application.routes.draw do
   delete '/logout' => 'sessions#destroy'
   get '/logged_in' => 'sessions#is_logged_in?'
   resources :users, only: [:create, :show, :index]
-  # test cookies function only
-  # get '/testuser' => 'users#test'
 
   resources :playrooms, only: [:index, :create, :destroy]
   get '/playrooms/:id' => 'playrooms#show'
-  # resources :messages, only: [:create]
 
   resources :draws
   mount ActionCable.server => '/cable'
