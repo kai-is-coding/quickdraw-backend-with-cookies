df0dbb8bd7fcf4b5c328611f4478238b4e2e72ba
Kai Yang <kaiyang.ky2@gmail.com>
2020-02-25 23:50:21 +1100

    added private websocket channel in backend

--- app/channels/draws_channel.rb
+++ app/channels/draws_channel.rb
@@ -1,27 +1,28 @@
 class DrawsChannel < ApplicationCable::Channel
   def subscribed
-    stream_from 'draw'  #"current_user_#{current_user.id}"
-    # stream_from "current_user_#{current_user.id}"
+    # @playroom = Playroom.find params[:id]
+    # stream_from 'draw'  #"current_user_#{current_user.id}"
+    stream_from "room_#{params[:playroom]}"
     puts "JOINED CHANNEL 'draw'"
 
-    ActionCable.server.broadcast 'draw', { msg: 'HELLO!' }
-    # ActionCable.server.broadcast "current_user_#{current_user.id}", { msg: 'HELLO!' }
+    # ActionCable.server.broadcast 'draw', { msg: 'HELLO!' }
+    ActionCable.server.broadcast "room_#{params[:playroom]}", { msg: 'HELLO!' }
   end
 
   # event from the frontend client
   def send_line(data)
     puts "GOT send_line() ----------------------------------"
     p data
-    ActionCable.server.broadcast 'draw', data
-    # ActionCable.server.broadcast "current_user_#{current_user.id}", data
+    # ActionCable.server.broadcast 'draw', data
+    ActionCable.server.broadcast "room_#{params[:playroom]}", data
   end
 
   def send_message(data)
     puts "GOT send_message() =================="
 
     p data
-    ActionCable.server.broadcast 'draw', data
-    # ActionCable.server.broadcast "current_user_#{current_user.id}", data
+    # ActionCable.server.broadcast 'draw', data
+    ActionCable.server.broadcast "room_#{params[:playroom]}", data
 
   end
 
