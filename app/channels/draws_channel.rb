8d228e875415592445b38d7f29766c011f17ed5f
Kai Yang <kaiyang.ky2@gmail.com>
2020-02-25 21:25:24 +1100

    chaned websocket setting for test

--- app/channels/draws_channel.rb
+++ app/channels/draws_channel.rb
@@ -1,25 +1,27 @@
 class DrawsChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from 'draw'  #"current_user_#{current_user.id}"
-    stream_from "current_user_#{current_user.id}"
+    stream_from 'draw'  #"current_user_#{current_user.id}"
+    # stream_from "current_user_#{current_user.id}"
     puts "JOINED CHANNEL 'draw'"
 
-    # ActionCable.server.broadcast 'draw', { msg: 'HELLO!' }
-    ActionCable.server.broadcast "current_user_#{current_user.id}", { msg: 'HELLO!' }
+    ActionCable.server.broadcast 'draw', { msg: 'HELLO!' }
+    # ActionCable.server.broadcast "current_user_#{current_user.id}", { msg: 'HELLO!' }
   end
 
   # event from the frontend client
   def send_line(data)
     puts "GOT send_line() ----------------------------------"
     p data
-    ActionCable.server.broadcast "current_user_#{current_user.id}", data
+    ActionCable.server.broadcast 'draw', data
+    # ActionCable.server.broadcast "current_user_#{current_user.id}", data
   end
 
   def send_message(data)
     puts "GOT send_message() =================="
 
     p data
-    ActionCable.server.broadcast "current_user_#{current_user.id}", data
+    ActionCable.server.broadcast 'draw', data
+    # ActionCable.server.broadcast "current_user_#{current_user.id}", data
 
   end
 
