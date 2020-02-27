e839dbada963db21fbf649199dfa272f053e7799
Kai Yang <kaiyang.ky2@gmail.com>
2020-02-27 11:03:08 +1100

    fixed cors

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,15 @@ module QuickdrawBackendWithCookies
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
+
+
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
     # Settings in config/environments/* take precedence over those specified here.
